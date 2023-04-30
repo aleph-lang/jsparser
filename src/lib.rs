fec2c2f3a710637519e5a173ef56802f86dffe0e
Roques Steve <steve.roques@gmail.com>
2023-04-30 19:36:49 +0200

    init js parser

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+use aleph_syntax_tree::syntax::AlephTree as at;
+
+/// Javascript parser
+/// #Arguments
+/// `source` - String to parse
+///
+/// # Return
+/// This function return an AlephTree
+pub fn python_parse(source: String) -> at {
+    //  println!("AST: {:?}", ast);
+    at::Unit
+}
+
+
