2123663d9112bb5e81625bfcaf56f785d3780063
Roques Steve <steve.roques@gmail.com>
2023-05-08 07:30:05 +0200

    fun decl, add, stmts added

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,85 @@
 use aleph_syntax_tree::syntax::AlephTree as at;
-use rslint_parser::{parse_text, Parse, SyntaxNode, SyntaxKind};
-use rslint_parser::ast::Script;
+use rslint_parser::{parse_text, SyntaxNode, SyntaxKind};
 
 fn is_operator(kind: SyntaxKind) -> bool {
     match kind {
-        SyntaxKind::PLUS | SyntaxKind::MINUS => true,
+        SyntaxKind::PLUS | SyntaxKind::MINUS | SyntaxKind::STAR | SyntaxKind::SLASH => true,
         _ => false
     }
 }
 
-fn process_node(node: &SyntaxNode) {
-    match node.kind() {
-        SyntaxKind::SCRIPT => {
-            println!("Found SCRIPT node");
+fn compute_stmt(node_list: Vec<at>) -> at {
+    let mut res = at::Unit;
+    for node in node_list {
+        res = match res {
+            at::Unit => node,
+            _ => at::Stmts{expr1: Box::new(res), expr2: Box::new(node)}
         }
-        SyntaxKind::EXPR_STMT => {
-            println!("Found EXPR_STMT node");
+    }
+    res
+
+}
+
+fn translate_ast(node: SyntaxNode) -> at {
+    match node.kind() {
+        SyntaxKind::SCRIPT | SyntaxKind::EXPR_STMT | SyntaxKind::BLOCK_STMT => {
+            compute_stmt(node.children().map(translate_ast).collect())
+        }
+        SyntaxKind::RETURN_STMT => {
+            compute_stmt(node.children().map(translate_ast).collect())
         }
         SyntaxKind::BIN_EXPR => {
             let operator = node
                 .children_with_tokens()
                 .find(|child| is_operator(child.kind()))
-                .unwrap();
-            let mut operands = node
+                .map(|token| token.kind())
+                .expect("Missing operator");
+            let operands: Vec<at> = node
                 .children()
-                .filter(|child| child.kind() == SyntaxKind::NAME_REF)
-                .map(|child| child.text().to_string());
-            println!("Found BIN_EXPR node {:?} {:?} {:?}", operands.next(), operator, operands.next());
+                .filter(|child| child.kind() == SyntaxKind::NAME_REF || child.kind().is_literal())
+                .map(translate_ast)
+                .collect();
+            match operator {
+                SyntaxKind::PLUS => at::Add{number_expr1: Box::new(operands[0].clone()), number_expr2: Box::new(operands[1].clone())},
+                SyntaxKind::MINUS => at::Sub{number_expr1: Box::new(operands[0].clone()), number_expr2: Box::new(operands[1].clone())},
+                SyntaxKind::STAR => at::Mul{number_expr1: Box::new(operands[0].clone()), number_expr2: Box::new(operands[1].clone())},
+                SyntaxKind::SLASH => at::Div{number_expr1: Box::new(operands[0].clone()), number_expr2: Box::new(operands[1].clone())},
+                _ => at::String{value : "Unknown".to_string()}
+            }
         }
         SyntaxKind::NAME_REF => {
             let name = node.text();
-            println!("Found NAME_REF node: {}", name);
+            at::Var{var: name.to_string(), is_pointer: "False".to_string()}
+        }
+        SyntaxKind::FN_DECL => {
+            let name = node
+                .children()
+                .find(|child| child.kind() == SyntaxKind::NAME)
+                .and_then(|child| Some(child.text()))
+                .expect("Missing function name")
+                .to_string();
+
+            let parameters: Vec<Box<at>> = node
+                .children()
+                .find(|child| child.kind() == SyntaxKind::PARAMETER_LIST)
+                .expect("Missing parameter list")
+                .children()
+                .map(|param| Box::new(at::Var{var: param.text().to_string(), is_pointer: "False".to_string()}))
+                .collect();
+
+            let body = node
+                .children()
+                .find(|child| child.kind() == SyntaxKind::BLOCK_STMT)
+                .map(|block| Box::new(translate_ast(block)))
+                .expect("Missing function body");
+
+            at::LetRec{name: name, args: parameters, body: body}
         }
         _ => {
             println!("Found other node {:?}", node.kind());
+            at::String{value: "Unknown".to_string()}
         }
     }
-
-    for child in node.children() {
-        process_node(&child);
-    }
-}
-
-fn translate(ast: Parse<Script>) -> at {
-    let root_node = ast.syntax();
-    process_node(&root_node);
-
-    at::Unit
 }
 
 /// Javascript parser
@@ -57,7 +90,10 @@ fn translate(ast: Parse<Script>) -> at {
 /// This function return an AlephTree
 pub fn parse(source: String) -> at {
     let ast = parse_text(&source,0);
-    translate(ast)   
+    //println!("AST {:?}", ast);
+
+    let root_node = ast.syntax();
+    translate_ast(root_node)
 }
 
 
