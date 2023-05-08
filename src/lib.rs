79ce5bcd7de8828e877faec73e5aa1ac5951e917
Roques Steve <steve.roques@gmail.com>
2023-05-08 19:08:58 +0200

    parse app, dot_expr, literal

--- src/lib.rs
+++ src/lib.rs
@@ -22,11 +22,30 @@ fn compute_stmt(node_list: Vec<at>) -> at {
 
 fn translate_ast(node: SyntaxNode) -> at {
     match node.kind() {
+        SyntaxKind::LITERAL | SyntaxKind::NAME_REF | SyntaxKind::NAME=> {
+            let name = node.text();
+            at::Var{var: name.to_string(), is_pointer: "False".to_string()}
+        }
         SyntaxKind::SCRIPT | SyntaxKind::EXPR_STMT | SyntaxKind::BLOCK_STMT => {
             compute_stmt(node.children().map(translate_ast).collect())
         }
         SyntaxKind::RETURN_STMT => {
-            compute_stmt(node.children().map(translate_ast).collect())
+            at::Return{value: Box::new(compute_stmt(node.children().map(translate_ast).collect()))}
+        }
+        SyntaxKind::UNARY_EXPR => {
+            let operator = node
+                .children_with_tokens()
+                .find(|child| is_operator(child.kind()))
+                .map(|token| token.kind())
+                .expect("Missing operator");
+            let operands: Vec<at> = node
+                .children()
+                .map(translate_ast)
+                .collect();
+            match operator {
+                SyntaxKind::MINUS => at::Neg{expr: Box::new(operands[0].clone())},
+                _ => at::String{value : "Unknown".to_string()}
+            }
         }
         SyntaxKind::BIN_EXPR => {
             let operator = node
@@ -36,7 +55,6 @@ fn translate_ast(node: SyntaxNode) -> at {
                 .expect("Missing operator");
             let operands: Vec<at> = node
                 .children()
-                .filter(|child| child.kind() == SyntaxKind::NAME_REF || child.kind().is_literal())
                 .map(translate_ast)
                 .collect();
             match operator {
@@ -46,10 +64,6 @@ fn translate_ast(node: SyntaxNode) -> at {
                 SyntaxKind::SLASH => at::Div{number_expr1: Box::new(operands[0].clone()), number_expr2: Box::new(operands[1].clone())},
                 _ => at::String{value : "Unknown".to_string()}
             }
-        }
-        SyntaxKind::NAME_REF => {
-            let name = node.text();
-            at::Var{var: name.to_string(), is_pointer: "False".to_string()}
         }
         SyntaxKind::FN_DECL => {
             let name = node
@@ -75,9 +89,41 @@ fn translate_ast(node: SyntaxNode) -> at {
 
             at::LetRec{name: name, args: parameters, body: body}
         }
+        SyntaxKind::CALL_EXPR => {
+            let callee = node 
+                .children()
+                .find(|child| child.kind() == SyntaxKind::DOT_EXPR)
+                .expect("Missing callee identifier")
+                .clone();
+
+            let parameters: Vec<Box<at>> = node
+                .children()
+                .filter(|child| child.kind() == SyntaxKind::ARG_LIST)
+                .flat_map(|arg_list| arg_list.children())
+                .map(|arg| Box::new(translate_ast(arg)))
+                .collect();
+
+            let at::App{object_name, fun, param_list: _} = translate_ast(callee) else { todo!() };
+            at::App{object_name: object_name, fun: fun, param_list: parameters} 
+        }
+        SyntaxKind::DOT_EXPR => {
+            if let Some(lhs) = node.children().next() {
+                if let Some(rhs) = node.children().nth(1) {
+                    at::App{object_name: lhs.to_string(), fun: Box::new(translate_ast(rhs)), param_list: Vec::new() }
+                } else {
+                    at::App{object_name: lhs.to_string(), fun: Box::new(at::Unit), param_list: Vec::new() }
+                } 
+            } else {
+                if let Some(rhs) = node.children().nth(1) {
+                    at::App{object_name: "".to_string(), fun: Box::new(translate_ast(rhs)), param_list: Vec::new() }
+                } else {
+                    at::App{object_name: "".to_string(), fun: Box::new(at::Unit), param_list: Vec::new() }
+                } 
+            }
+        }
         _ => {
             println!("Found other node {:?}", node.kind());
-            at::String{value: "Unknown".to_string()}
+            at::String{value: "Unknown_node".to_string()}
         }
     }
 }
