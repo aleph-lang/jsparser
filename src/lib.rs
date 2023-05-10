5f500f96d9ca19d6ad0620a9434e40f20b26fcda
Roques Steve <steve.roques@gmail.com>
2023-05-10 20:45:25 +0200

    if, eq, neq, var, let added

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 use aleph_syntax_tree::syntax::AlephTree as at;
-use rslint_parser::{parse_text, SyntaxNode, SyntaxKind};
+use rslint_parser::{parse_text, SyntaxNode};
+use rslint_parser::SyntaxKind as sk;
 
-fn is_operator(kind: SyntaxKind) -> bool {
+fn is_operator(kind: sk) -> bool {
     match kind {
-        SyntaxKind::PLUS | SyntaxKind::MINUS | SyntaxKind::STAR | SyntaxKind::SLASH 
-        | SyntaxKind::AMP | SyntaxKind::PIPE | SyntaxKind::BANG => true,
+        sk::PLUS | sk::MINUS | sk::STAR | sk::SLASH 
+            | sk::EQ | sk::EQ2 | sk::EQ3 | sk::NEQ | sk::NEQ2 
+            | sk::AMP | sk::PIPE | sk::BANG => true,
         _ => false
     }
 }
@@ -23,11 +25,14 @@ fn compute_stmt(node_list: Vec<at>) -> at {
 
 fn translate_ast(node: SyntaxNode) -> at {
     match node.kind() {
-        SyntaxKind::NAME_REF | SyntaxKind::NAME => {
+        sk::EMPTY_STMT => {
+            at::Unit
+        }
+        sk::NAME_REF | sk::NAME => {
             let name = node.text();
             at::Var{var: name.to_string(), is_pointer: "False".to_string()}
         }
-        SyntaxKind::LITERAL => {
+        sk::LITERAL => {
             let value = node.text();
             let f = value.to_string().parse::<f64>();
             match f {
@@ -47,13 +52,13 @@ fn translate_ast(node: SyntaxNode) -> at {
                 }
             }
         }
-        SyntaxKind::SCRIPT | SyntaxKind::EXPR_STMT | SyntaxKind::BLOCK_STMT | SyntaxKind::GROUPING_EXPR => {
+        sk::SCRIPT | sk::EXPR_STMT | sk::BLOCK_STMT | sk::GROUPING_EXPR => {
             compute_stmt(node.children().map(translate_ast).collect())
         }
-        SyntaxKind::RETURN_STMT => {
+        sk::RETURN_STMT => {
             at::Return{value: Box::new(compute_stmt(node.children().map(translate_ast).collect()))}
         }
-        SyntaxKind::UNARY_EXPR => {
+        sk::UNARY_EXPR => {
             let operator = node
                 .children_with_tokens()
                 .find(|child| is_operator(child.kind()))
@@ -64,12 +69,12 @@ fn translate_ast(node: SyntaxNode) -> at {
                 .map(translate_ast)
                 .collect();
             match operator {
-                SyntaxKind::MINUS => at::Neg{expr: Box::new(operands[0].clone())},
-                SyntaxKind::BANG => at::Not{bool_expr: Box::new(operands[0].clone())},
+                sk::MINUS => at::Neg{expr: Box::new(operands[0].clone())},
+                sk::BANG => at::Not{bool_expr: Box::new(operands[0].clone())},
                 _ => at::String{value : "Unknown".to_string()}
             }
         }
-        SyntaxKind::BIN_EXPR => {
+        sk::BIN_EXPR => {
             let operator = node
                 .children_with_tokens()
                 .find(|child| is_operator(child.kind()))
@@ -80,26 +85,31 @@ fn translate_ast(node: SyntaxNode) -> at {
                 .map(translate_ast)
                 .collect();
             match operator {
-                SyntaxKind::PLUS => at::Add{number_expr1: Box::new(operands[0].clone()), number_expr2: Box::new(operands[1].clone())},
-                SyntaxKind::MINUS => at::Sub{number_expr1: Box::new(operands[0].clone()), number_expr2: Box::new(operands[1].clone())},
-                SyntaxKind::STAR => at::Mul{number_expr1: Box::new(operands[0].clone()), number_expr2: Box::new(operands[1].clone())},
-                SyntaxKind::SLASH => at::Div{number_expr1: Box::new(operands[0].clone()), number_expr2: Box::new(operands[1].clone())},
-                SyntaxKind::AMP => at::And{bool_expr1: Box::new(operands[0].clone()), bool_expr2: Box::new(operands[1].clone())},
-                SyntaxKind::PIPE => at::Or{bool_expr1: Box::new(operands[0].clone()), bool_expr2: Box::new(operands[1].clone())},
+                sk::PLUS => at::Add{number_expr1: Box::new(operands[0].clone()), number_expr2: Box::new(operands[1].clone())},
+                sk::MINUS => at::Sub{number_expr1: Box::new(operands[0].clone()), number_expr2: Box::new(operands[1].clone())},
+                sk::STAR => at::Mul{number_expr1: Box::new(operands[0].clone()), number_expr2: Box::new(operands[1].clone())},
+                sk::SLASH => at::Div{number_expr1: Box::new(operands[0].clone()), number_expr2: Box::new(operands[1].clone())},
+                sk::AMP => at::And{bool_expr1: Box::new(operands[0].clone()), bool_expr2: Box::new(operands[1].clone())},
+                sk::PIPE => at::Or{bool_expr1: Box::new(operands[0].clone()), bool_expr2: Box::new(operands[1].clone())},
+                sk::EQ => at::Eq{expr1: Box::new(operands[0].clone()), expr2: Box::new(operands[1].clone())},
+                sk::EQ2 => at::Eq{expr1: Box::new(operands[0].clone()), expr2: Box::new(operands[1].clone())},
+                sk::EQ3 => at::Eq{expr1: Box::new(operands[0].clone()), expr2: Box::new(operands[1].clone())},
+                sk::NEQ => at::Not{bool_expr: Box::new(at::Eq{expr1: Box::new(operands[0].clone()), expr2: Box::new(operands[1].clone())})},
+                sk::NEQ2 => at::Not{bool_expr: Box::new(at::Eq{expr1: Box::new(operands[0].clone()), expr2: Box::new(operands[1].clone())})},
                 _ => at::String{value : "Unknown".to_string()}
             }
         }
-        SyntaxKind::FN_DECL => {
+        sk::FN_DECL => {
             let name = node
                 .children()
-                .find(|child| child.kind() == SyntaxKind::NAME)
+                .find(|child| child.kind() == sk::NAME)
                 .and_then(|child| Some(child.text()))
                 .expect("Missing function name")
                 .to_string();
 
             let parameters: Vec<Box<at>> = node
                 .children()
-                .find(|child| child.kind() == SyntaxKind::PARAMETER_LIST)
+                .find(|child| child.kind() == sk::PARAMETER_LIST)
                 .expect("Missing parameter list")
                 .children()
                 .map(|param| Box::new(at::Var{var: param.text().to_string(), is_pointer: "False".to_string()}))
@@ -107,22 +117,22 @@ fn translate_ast(node: SyntaxNode) -> at {
 
             let body = node
                 .children()
-                .find(|child| child.kind() == SyntaxKind::BLOCK_STMT)
+                .find(|child| child.kind() == sk::BLOCK_STMT)
                 .map(|block| Box::new(translate_ast(block)))
                 .expect("Missing function body");
 
             at::LetRec{name: name, args: parameters, body: body}
         }
-        SyntaxKind::CALL_EXPR => {
+        sk::CALL_EXPR => {
             let callee = node 
                 .children()
-                .find(|child| child.kind() == SyntaxKind::DOT_EXPR)
+                .find(|child| child.kind() == sk::DOT_EXPR)
                 .expect("Missing callee identifier")
                 .clone();
 
             let parameters: Vec<Box<at>> = node
                 .children()
-                .filter(|child| child.kind() == SyntaxKind::ARG_LIST)
+                .filter(|child| child.kind() == sk::ARG_LIST)
                 .flat_map(|arg_list| arg_list.children())
                 .map(|arg| Box::new(translate_ast(arg)))
                 .collect();
@@ -130,7 +140,7 @@ fn translate_ast(node: SyntaxNode) -> at {
             let at::App{object_name, fun, param_list: _} = translate_ast(callee) else { todo!() };
             at::App{object_name: object_name, fun: fun, param_list: parameters} 
         }
-        SyntaxKind::DOT_EXPR => {
+        sk::DOT_EXPR => {
             if let Some(lhs) = node.children().next() {
                 if let Some(rhs) = node.children().nth(1) {
                     at::App{object_name: lhs.to_string(), fun: Box::new(translate_ast(rhs)), param_list: Vec::new() }
@@ -144,6 +154,41 @@ fn translate_ast(node: SyntaxNode) -> at {
                     at::App{object_name: "".to_string(), fun: Box::new(at::Unit), param_list: Vec::new() }
                 } 
             }
+        }
+        sk::VAR_DECL => {
+            if let Some(identifier) = node.children().next() {
+                at::Var{var: identifier.text().to_string(), is_pointer: "False".to_string()}
+            } else { todo!() }
+        }
+        sk::ASSIGN_EXPR => {
+            if let Some(lhs) = node.children().next() {
+                if let Some(rhs) = node.children().nth(1) {
+                    at::Let{var: lhs.text().to_string(), is_pointer: "False".to_string(), value: Box::new(translate_ast(rhs)), expr: Box::new(at::Unit)}
+                } else { todo!() }
+            } else { todo!() }
+        }
+        sk::IF_STMT => {
+            let branches: Vec<Box<at>> = node.children()
+                .filter(|child| child.kind() == sk::BLOCK_STMT)
+                .map(|arg| Box::new(translate_ast(arg)))
+                .collect();
+
+            if let Some(condition) = node.children().find(|child| child.kind() == sk::CONDITION) {
+                let mut thn = Box::new(at::Unit);
+                if let Some(then_ast) = branches.get(0) {
+                    thn = then_ast.clone()
+                }
+                let mut els = Box::new(at::Unit);
+                if let Some(els_ast) = branches.get(1) {
+                    els = els_ast.clone()
+                }
+                at::If{condition: Box::new(translate_ast(condition)), then: thn, els: els}
+            } else { todo!() }
+        }
+        sk::CONDITION => {
+            if let Some(condition) = node.children().next() {
+                translate_ast(condition)
+            } else { todo!() }
         }
         _ => {
             println!("Found other node {:?}", node.kind());
